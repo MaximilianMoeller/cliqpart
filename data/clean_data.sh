e9d9836cd39c69c0b2271228620fc292872eb4f6
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2023-11-09 16:00:03 +0100

    added wild-cats data from grötschel wakabayashi

--- data/clean_data.sh
+++ data/clean_data.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 find . -name "optimal.sol" -exec rm '{}' \;
+find . -name "optimal.log" -exec rm '{}' \;
+find . -name "gurobi.log" -exec rm '{}' \;
 find . -name "measurements.csv" -exec rm '{}' \;
